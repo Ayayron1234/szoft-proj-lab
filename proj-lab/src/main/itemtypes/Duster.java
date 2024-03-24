94c8943f8372f62b136a28b93cbec897b42004a8
Ayayron1234 <bertalanaron01@gmail.com>
2024-03-24 23:16:37 +0100

    changes in duster and camembertTest

--- proj-lab/src/main/itemtypes/Duster.java
+++ proj-lab/src/main/itemtypes/Duster.java
@@ -1,6 +1,9 @@
 package main.itemtypes;
 
+import main.Entity;
 import main.Item;
+import main.Room;
+import main.TimerEvent;
 
 public class Duster extends Item {
     private int usesLeft = 2;
@@ -9,4 +12,28 @@ public class Duster extends Item {
     public String GetName() {
         return "Nedves Táblatörlő Rongy";
     }
+
+    /**
+     * @param who
+     * @param where
+     */
+    @Override
+    public void PickedUp(Entity who, Room where) {
+
+    }
+
+    /**
+     * @param who
+     * @param where
+     */
+    @Override
+    public void Placed(Entity who, Room where) {
+
+    }
+
+    @Override
+    public void StartRound(TimerEvent data) {
+
+    }
+
 }
